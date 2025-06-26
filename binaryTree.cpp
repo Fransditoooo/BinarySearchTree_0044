f9637ce643437caf737597535fc5443ae4adffc1
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-06-26 11:55:27 +0700

    step 3

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -38,4 +38,8 @@ public:
         //Step 2: Assign value to the data field of new node
         newNode->info = x;
 
+        //Step 3: Make the left and right child of the new node point
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
         
